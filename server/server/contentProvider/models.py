292c693de331ddfa9aad3c53c78b9469b4f0f9a3
Maciej Foks <foks.maciej@gmail.com>
2014-10-26 01:54:49 +0200

    model

--- server/server/contentProvider/models.py
+++ server/server/contentProvider/models.py
@@ -2,11 +2,23 @@ from django.db import models
 
 # Create your models here.
 
+class HashTag(models.Model):
+    tag = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.tag
+
 class MovieToGuess(models.Model):
     userId = models.IntegerField()
+    name = models.CharField(max_length=100)
     
     movie = models.FileField(upload_to = 'movies')
     ending = models.FileField(upload_to = 'movies')
 
     guessA = models.CharField(max_length=300)
     guessB = models.CharField(max_length=300)
+
+    hashtags = models.ManyToMany(Hashtag)
+    
+    def __str__(self):
+        return self.name
