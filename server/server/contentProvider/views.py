a363eb7875c9b21b981cb2c9eaef77750a797f50
Grzegorz Szpak <gszpak@student.uw.edu.pl>
2014-10-26 02:21:32 +0200

    Tagged videos view added

--- server/server/contentProvider/views.py
+++ server/server/contentProvider/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from models import MovieToGuess
+import json
 
 # Create your views here.
 
+def tagged_videos(request, tag=None):
+    tagged_vids = MovieToGuess.objects.filter(tags__name=tag)
+    urls = [tagged.url for tagged in tagged_vids]
+    return HttpResponse(
+        json.dumps({'videos' : urls}),
+        content_type="application/json"
+    )
