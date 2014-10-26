8c2ad5a3fcf197b2090ea13489ffb5ceefb705fa
Grzegorz Szpak <gszpak@student.uw.edu.pl>
2014-10-26 04:20:20 +0100

    Bugfixes

--- server/server/contentProvider/viewTests.py
+++ server/server/contentProvider/viewTests.py
@@ -1,11 +1,11 @@
 import requests
 import json
 
-url = 'http://127.0.0.1:8000/'
+url = 'http://127.0.0.1:8000/upload'
 
 files = {'movie': open('cos.mov', 'rb')}
 data = {'userid' : 1, 'name' : 'kupacz', 'question' : 'co jest?',
-    'second' : '2', 'guessA' : 'A', 'guessB' : 'B', 'hashtags' : '[]'}
+    'stopTime' : '2', 'goodAnswer' : 'A', 'badAnswer' : 'B', 'hashtags' : '[]'}
 
 def go_gg():
     requests.post(url, files=files, data=data)
